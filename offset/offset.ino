5ae456150bccd39aa9cf5edd9fccdebbc1053249
Eli Ferin <eli.ferin@studenti.unipd.it>
2023-12-08 17:11:17 +0100

    Create offset.ino

---
+++ offset/offset.ino
@@ -0,0 +1,26 @@
+#include "AS5600.h"
+#include "Wire.h"
+
+// Arduino UNO: SCL:A5 SDA:A4
+AS5600 encoder;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // Increase the baudrate to speed things up
+  Serial.begin(115200);
+  Serial.println("\n===================================\nProgram started");
+
+  // From the library example 
+  Wire.begin();
+  Wire.setClock(800000);
+  encoder.begin(2);
+  encoder.setDirection(AS5600_CLOCK_WISE);
+  Serial.print("AS5600 connect: "); Serial.println(encoder.isConnected());
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(encoder.readAngle() * AS5600_RAW_TO_DEGREES);
+}
